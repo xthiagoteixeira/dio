764ef608f03d380d4b63360516f3db5ee2db579b
Thiago <xthiagoteixeira@proton.me>
2024-05-23 10:30:26 -0300

    Desafio Java-Santander

--- java-santander/contabancaria/src/main/java/org/dio/contabancaria/ContaTerminal.java
+++ java-santander/contabancaria/src/main/java/org/dio/contabancaria/ContaTerminal.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.dio.contabancaria;
 
-/**
- *
- * @author xthia
- */
+// Conhecer e importar a classe Scanner
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner ler = new Scanner(System.in);
+
+        // Exibir as mensagens para o nosso usuário
+        // Obter pela Scanner os valores digitados no terminal
+        System.out.println("\n Informe o Nro da Conta:     ");
+        int nroConta = ler.nextInt();
+
+        System.out.println("\n Informe o Nro da Agência:   ");
+        String nroAgencia = ler.next();
+
+        System.out.println("\n Informe o Nome do Cliente:  ");
+        String nomeCliente = ler.next();
+
+        System.out.println("\n Informe o Saldo:            ");
+        double saldo = ler.nextDouble();
+        
+        // Exibir a mensagem conta criada
+        System.out.printf("\n Conta: %d", nroConta);
+        System.out.printf("\n Agência: %s", nroAgencia);
+        System.out.printf("\n Nome do Cliente: %s", nomeCliente);
+        System.out.printf("\n Saldo: %.2f", saldo);
+        
     }
 }
