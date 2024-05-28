64983d43ac8dd40abf498e9075fde43f87b9f3ea
Thiago <xthiagoteixeira@proton.me>
2024-05-28 15:37:01 -0300

    Controle de Fluxo

---
+++ java-santander/controle-fluxo/src/main/java/ExemploFor_Arrays.java
@@ -0,0 +1,11 @@
+public class ExemploFor_Arrays {
+
+    public static void main(String[] args) {
+        for ( int carneirinhos =1; carneirinhos<=20; carneirinhos++){
+            System.out.println("Contando carneirinhos: " + carneirinhos);
+
+        }
+        System.out.println("Joaozinho dormiu");
+    }
+
+}
