54f95bfb76ccd2459b214100deaf7d6b97eba45e
Thiago <xthiagoteixeira@proton.me>
2024-05-29 18:59:20 -0300

    Controle de Fluxo

---
+++ java-santander/controle-fluxo/src/main/java/ExemploBreakContinue.java
@@ -0,0 +1,18 @@
+public class ExemploBreakContinue {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if(i==3)
+                break;
+                //continue;
+
+            System.out.println(i);
+
+        }
+
+
+    }
+
+}
--- java-santander/controle-fluxo/src/main/java/ExemploFor_Arrays.java
+++ java-santander/controle-fluxo/src/main/java/ExemploFor_Arrays.java
@@ -1,11 +1,16 @@
 public class ExemploFor_Arrays {
 
     public static void main(String[] args) {
-        for ( int carneirinhos =1; carneirinhos<=20; carneirinhos++){
-            System.out.println("Contando carneirinhos: " + carneirinhos);
+        String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
 
+        //Forma abreviada
+        //for(String aluno : alunos)
+        //System.out.println("Nome do aluno é: " +aluno);
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
         }
-        System.out.println("Joaozinho dormiu");
+
     }
 
 }
