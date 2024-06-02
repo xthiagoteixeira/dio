aecc4add35f839b2e06e761dae6764fbf441dbb0
Thiago <xthiagoteixeira@proton.me>
2024-06-02 13:28:41 -0300

    Processo Seletivo

---
+++ java-santander/candidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,25 @@
+public class ProcessoSeletivo {
+
+    public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+        analisarCandidato(1800.0);
+        analisarCandidato(2300.0);
+        analisarCandidato(2000.0);
+
+
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+
+        double salarioBase = 2000.0;
+        if(salarioBase>salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase==salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+
+    }
+
+}
