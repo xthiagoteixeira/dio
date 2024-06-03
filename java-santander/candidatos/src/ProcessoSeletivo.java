1a56881562557e30de71c8ce0c65c9b6dcb85cc2
Thiago <xthiagoteixeira@proton.me>
2024-06-03 17:53:40 -0300

    Processo Seletivo

--- java-santander/candidatos/src/ProcessoSeletivo.java
+++ java-santander/candidatos/src/ProcessoSeletivo.java
@@ -6,12 +6,12 @@ public class ProcessoSeletivo {
         analisarCandidato(2300.0);
         analisarCandidato(2000.0);
 
-
     }
 
     static void analisarCandidato(double salarioPretendido){
 
         double salarioBase = 2000.0;
+
         if(salarioBase>salarioPretendido){
             System.out.println("LIGAR PARA O CANDIDATO");
         }else if(salarioBase==salarioPretendido){
---
+++ java-santander/candidatos/src/ProcessoSeletivo_Selecionando.java
@@ -0,0 +1,90 @@
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo_Selecionando {
+    public static String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+    public static String [] candidatosResultado = {"","","","",""};
+    public static double salarioBase = 2000.0;
+
+    public static void main(String[] args) {
+        selecaoCandidatos();
+        imprimirSelecionados();
+        for (String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(string candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do {
+            atendeu=atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+        }while(continuarTentando && tentativasRealizadas<3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas);
+        else
+            System.out.println("NAO CONSEGUIMOS CONTATO COM " + candidato + " NUMERO MAXIMO DE TENTATIVAS " + tentativasRealizadas);
+
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
+    static void imprimirSelecionados(){
+        System.out.println("................................");
+        System.out.println("Imprimindo a lista de candidatos");
+
+        for(String candidato: candidatosResultado){
+            if(!Objects.equals(candidato, "")){
+                System.out.println("O candidato SELECIONADO foi: " + candidato);
+            }
+        }
+    }
+
+    static void selecaoCandidatos(){
+        int candidatosAtual = 0;
+        int candidatosSelecionados = 0;
+
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou o salario " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosResultado[candidatosSelecionados] = candidato;
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    void analisarCandidato(double salarioPretendido){
+
+         if(salarioBase>salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase==salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+
+    }
+
+}
