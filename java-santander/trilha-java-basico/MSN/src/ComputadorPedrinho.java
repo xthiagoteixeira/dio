bcc39eceb6898d2b3c3beca06882ea5546fec49e
xthiagoteixeira <thiagosegall@gmail.com>
2024-06-10 11:06:41 -0300

    POO

--- java-santander/trilha-java-basico/MSN/src/ComputadorPedrinho.java
+++ java-santander/trilha-java-basico/MSN/src/ComputadorPedrinho.java
@@ -1,19 +1,23 @@
 public class ComputadorPedrinho {
     public static void main(String[] args) {
-        System.out.println("MSN");
-        MSNMessenger msn = new MSNMessenger();
-        msn.enviarMensagem();
-        msn.receberMensagem();
 
-        System.out.println("FACEBOOK");
-        FacebookMessenger fcb = new FacebookMessenger();
-        fcb.enviarMensagem();
-        fcb.receberMensagem();
+        ServicoMensagemInstantanea smi = null;
 
-        System.out.println("TELEGRAM");
-        Telegram tlg = new Telegram();
-        tlg.enviarMensagem();
-        tlg.receberMensagem();
+		/*
+		    NÃO SE SABE QUAL APP
+		    MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+		 */
+        String appEscolhido="???";
 
+        if(appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
+        else if(appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+        else if(appEscolhido.equals("tlg"))
+            smi = new Telegram();
+
+
+        smi.enviarMensagem();
+        smi.receberMensagem();
     }
 }
